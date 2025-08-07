239bc6e5a02cd282c03d6a4a71e44422f9355dc3
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2025-08-07 12:15:07 +0530

    Linq to SQL

--- SampleWinConsole/Ex04linqExample.cs
+++ SampleWinConsole/Ex04linqExample.cs
@@ -58,13 +58,140 @@ namespace SampleWinConsole
     internal class Ex04linqExample
     {
         static List<Employee> masterData = new EmployeeDB().GetAllEmployees();
+        static List<Dept> masterDepts = new EmployeeDB().GetAllDepts();
+
         static void Main(string[] args)
         {
             //extensionmethodUsage();
 
             //readonlyDescriptions();
             //readDescAndDate();
-            readNamesAndSalarys();
+            //readNamesAndSalarys();
+            //readNamesFromAddress("San Jose");
+            //getEmployeesByGroups();
+            //groupEmployeesAlphabettically();
+            //joinExample();
+            //joinExampleGroupedByDept();
+            joinAllEmployeesWithDepts();
+        }
+
+        private static void joinAllEmployeesWithDepts()
+        {
+            var results = from emp in masterData
+                          join dept in masterDepts
+                          on emp.DeptId equals dept.DeptId into gr
+                          from subDept in gr.DefaultIfEmpty() // This is a left outer join.
+                          select new
+                          {
+                              emp.EmpName,
+                              emp.EmpSalary,
+                              emp.EmpAddress,
+                              DeptName = subDept?.DeptName ?? "No Department"
+                          };
+            foreach(var emp in results)
+            {
+                Console.WriteLine($"{emp.EmpName} is working in {emp.DeptName}");
+            }
+            //defaultIfEmpty() ensure that if no matching record is found in the Dept table, it will return a null value for that record.  
+        }
+
+        private static void joinExampleGroupedByDept()
+        {
+            //todo: Create a join between Employee and Dept tables and group the results by DeptName. 
+            var results = from emp in masterData
+                          join dept in masterDepts
+                          on emp.DeptId equals dept.DeptId
+                          group new { emp, dept } by dept.DeptName
+                          into gr
+                          orderby gr.Key ascending
+                          select gr;
+            foreach(var group in results)
+            {
+                Console.WriteLine("Employees under: " + group.Key);
+                foreach(var rec in group)
+                {
+                    Console.WriteLine($"{rec.emp.EmpName} earns {rec.emp.EmpSalary}");
+                }
+                Console.WriteLine("\n\n\n");
+            }
+        }
+
+        /// <summary>
+        /// Example of EquiJoin using LINQ.
+        /// </summary>
+        private static void joinExample()
+        {
+            var results = from emp in masterData join dept in masterDepts 
+                          on emp.DeptId equals dept.DeptId
+                          select new
+                          {
+                              emp.EmpName,
+                              emp.EmpSalary,
+                              emp.EmpAddress,
+                              dept.DeptName
+                          };
+            foreach(var emp in results)
+            {
+                Console.WriteLine($"{emp.EmpName} from {emp.DeptName}");
+            }
+        }
+
+        private static string GetSecondOrFirstName(string empName)
+        {
+            int firstSpace = empName.IndexOf(' ');
+            if(firstSpace < 0) return empName;
+            int secondSpace = empName.IndexOf(' ', firstSpace + 1);
+            if(secondSpace < 0)
+                return empName.Substring(firstSpace + 1);
+            return empName.Substring(firstSpace + 1, secondSpace - firstSpace - 1);
+        }
+        private static void groupEmployeesAlphabettically()
+        {
+            var groups = from emp in masterData
+                         orderby (emp.EmpName.Split(' ').Length > 1 ? emp.EmpName.Split(' ')[1] : emp.EmpName.Split(' ')[0]) ascending
+                         group emp by emp.EmpName[0]
+                         into gr
+                         orderby gr.Key ascending
+                         select gr;
+            foreach(var group in groups)
+            {
+                Console.WriteLine("Employee names under " + group.Key);
+                foreach(var emp in group)
+                {
+                    Console.WriteLine($"{emp.EmpName} earns {emp.EmpSalary:C}");
+                }
+            }
+        }
+
+        private static void getEmployeesByGroups()
+        {
+            var results = from emp in masterData
+                          orderby emp.EmpName
+                          group emp.EmpName by emp.EmpAddress
+                          into gr
+                          orderby gr.Key
+                          select gr;
+            foreach(var gr in results)
+            {
+                Console.WriteLine("Employees in " + gr.Key);
+                foreach(var name in gr)
+                {
+                    Console.Write($"{name},  ");
+                }
+                Console.WriteLine("\n\n");
+            }
+            //todo: Create groups based on Alphabets of the FirstName. 
+        }
+
+        private static void readNamesFromAddress(string v)
+        {
+            Console.WriteLine($"Master Count: {masterData.Count}");
+            var results = from emp in masterData
+                          where emp.EmpAddress.ToLower() == v.ToLower() && emp.EmpSalary > 75000
+                          select new { Name = emp.EmpName, Salary = emp.EmpSalary };
+            //LINQ executes only in the foreach statement, not before it.
+            Console.WriteLine($"Where count: {results.Count()}");
+            foreach(var res in results) Console.WriteLine(res);
         }
 
         private static void readNamesAndSalarys()
---
+++ SampleWinConsole/Ex06LinqToSqlDemo.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//LINQ to SQL is used to query databases using LINQ syntax. This was the first ORM (Object Relational Mapping) technology introduced by Microsoft. Currently, it is not actively developed, and Entity Framework is the recommended ORM technology. However, LINQ to SQL is still supported in .NET Core and .NET 5+. LINQ to SQL works with SQL Server databases and allows you to map database tables to C# classes, enabling you to work with data in a more object-oriented way.
+//Server Explorer is configured with UR Database. 
+//Add LINQ to SQL Classes item to the project.
+//In the designer, drag and drop the tables you want to work with.
+// This will generate the necessary classes and context for LINQ to SQL.
+//DataContext is the class that represents the database connection and provides access to the tables in the form of properties. Every table in the database will have a corresponding property in the DataContext class in a pluralized name.
+namespace SampleWinConsole
+{
+    using SampleWinConsole.LinqData;
+    internal class Ex06LinqToSqlDemo
+    {
+        static void Main(string[] args)
+        {
+            displayAll();
+            //insertEmployee();
+            //updateEmployee();
+            //todo: Handle the delete operation also.
+            //showEmpWithDepts();
+            showEmployeesOfDept("Training");
+        }
+
+        private static void showEmployeesOfDept(string v)
+        {
+            var context = new EmpDbDataContext();
+            var records = context.DeptTables.FirstOrDefault(dept => dept.DeptName == v).Employees;
+            foreach(var rec in records)
+            {
+                Console.WriteLine(rec.EmpName);
+            }
+        }
+
+        private static void showEmpWithDepts()
+        {
+            //create context
+            var context = new EmpDbDataContext();
+            //query the Employees table and join with DeptTable to get the department name.
+            var records = from emp in context.Employees
+                          join dept in context.DeptTables
+                          on emp.DeptId equals dept.DeptId //Join condition
+                          select new
+                          {
+                             Name = emp.EmpName,
+                             Dept = dept.DeptName,
+                             Salary = emp.EmpSalary
+                          };
+            //display the data
+            foreach(var item in records)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        private static void updateEmployee()
+        {
+            //create the Dbcontext.
+            var context = new EmpDbDataContext();
+            //find the matching record to updaate
+            var rec = context.Employees.FirstOrDefault(emp => emp.EmpId == 1); //Assuming EmpId 1 exists in the Employees table.
+            //update the values
+            rec.EmpName = "Phaniraj B.N.";
+            rec.EmpSalary = 78000;
+            //save the changes.
+            context.SubmitChanges(); //SubmitChanges is used to save the changes to the database.
+        }
+
+        private static void insertEmployee()
+        {
+            //As EmpID is auto generated, we dont need to set it.
+            var emp = new Employee //Employee of LINQ DBContext
+            {
+                EmpName = "Phaniraj",
+                EmpAddress = "Bangalore",
+                EmpSalary = 60000,
+                DeptId = 1 //Assuming DeptId 1 exists in the DeptTable
+            };
+            var context = new EmpDbDataContext();
+            context.Employees.InsertOnSubmit(emp); //InsertOnSubmit is used to add a new record to the Employees table.
+            context.SubmitChanges(); //SubmitChanges is used to save the changes to the database.
+        }
+
+        private static void displayAll()
+        {
+            var context = new EmpDbDataContext();
+            //var records = from emp in context.Employees
+            //              select emp; //Select * from Employee
+            //foreach(var emp in records)
+            //{
+            //    Console.WriteLine(emp.EmpName);
+            //}
+
+            var depts = from dept in context.DeptTables
+                        select dept; //Select * from DeptTable
+            foreach(var item in depts)
+            {
+                Console.WriteLine(item.DeptName);
+            }
+
+
+        }
+    }
+}
