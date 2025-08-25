495daf8b7b44d4b8cce7c8d9441b57722091dbd4
Phani-TrainingPrograms <131134278+Phani-TrainingPrograms@users.noreply.github.com>
2025-08-25 10:12:25 +0530

    .NET CORE EF Examples

---
+++ DotnetCoreApps/SampleDotnetCoreApp/Data/DBComponents.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SampleDotnetCoreApp.Data
+{
+    //In this Example, we are using EF's Code -First approach to define a simple data model for a library system. The model consists of a Entity class called Book that represents real world book. 
+    //Like LINQ to SQL, EF Core allows us to query and manipulate data using C# objects and LINQ queries. It uses DBContext class to manage the database connection and perform CRUD operations on the data.
+    //Attributes like [Key] and [Required] are used to define the primary key and required properties of the Book entity.
+    [Table("Books")]
+    internal class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]//NOT NULL
+        [MaxLength(100)]
+        public string Title { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public int BookPrice { get; set; }
+    }
+
+    class BookContext : DbContext
+    {
+        //"Data Source =.\SQLEXPRESS; Initial Catalog = FnfTraining; Integrated Security = True; Encrypt = False; Trust Server Certificate=True"
+        public DbSet<Book> MyBooks { get; set; }
+
+        private const string DB_SOURCE = ".\\SQLEXPRESS"; //Your SQL Server instance name;
+        private const string DB_NAME = "FnfTraining";
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            var connectionString = $"Data Source={DB_SOURCE};Initial Catalog={DB_NAME};Integrated Security=True;Encrypt=False; Trust Server Certificate=True";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
+//Open the Package Manager Console in Visual Studio and run the following commands to create and apply a migration:
+// PM> Add-Migration mig1
+// PM> Update-Database
---
+++ DotnetCoreApps/SampleDotnetCoreApp/Readme.md
@@ -0,0 +1,21 @@
+﻿# Entity Framework Core 8.0 with .NET 8.0
+## Steps to create and run a sample application
+1. Create a new .NET 8.0 console application:
+   ```bash
+   dotnet new console -n SampleDotnetCoreApp
+   cd SampleDotnetCoreApp
+   ```
+2. Add the required NuGet packages for Entity Framework Core:
+   ```bash
+   dotnet add package Microsoft.EntityFrameworkCore
+   dotnet add package Microsoft.EntityFrameworkCore.SqlServer
+   dotnet add package Microsoft.EntityFrameworkCore.Tools
+   ```
+3. Create the data model and DbContext class in the Data folder as explained. Code is shared in the Data folder.
+4. Open the Package Manager CLI and run the following commands to create and apply migrations:
+   ```
+   add-migration InitialCreate
+   update-database	
+   ``` 
+1. In the Program.cs file create the instance of the DBContext object, instance of the Book object that U want to insert into the database and add the code as mentioned in the Program.cs file
+5. Run the application to test the results. 
